b9e483858703bdf73ff2888e400be9d99a23e2e1
jrod454 <jrod454@gmail.com>
2020-10-18 15:40:50 -0500

    Added the ability to list the settings.

--- config/settings.js
+++ config/settings.js
@@ -3,6 +3,13 @@ const db = new Firestore({
     projectId: 'sofia-1b7ee',
     keyFilename: 'sofia-1b7ee.json',
 });
+
+//Used for testing
+// const db = new Firestore({
+//     projectId: 'helios-6fbe5',
+//     keyFilename: 'helios-6fbe5.json',
+// });
+
 module.exports.db = db;
 
 const Discord = require("discord.js");
@@ -29,6 +36,8 @@ module.exports.execute = async (parsedMessage, message, database) => {
             if (parsedMessage.arguments[0] === "refresh") {
                 await this.init();
                 utils.sendMessage(message.channel.id, `Refreshed settings`);
+            } else if (parsedMessage.arguments[0] === "list") {
+                listSettings(message.guild.id, message.channel.id);
             } else {
                 throw `Invalid settings command`;
             }
@@ -55,6 +64,24 @@ module.exports.execute = async (parsedMessage, message, database) => {
         default:
             throw `Unknown settings command`
     }
+};
+
+listSettings = (guildId, channelId) => {
+    let output = `Settings:\n`;
+    let totalSettings = new Map();
+    for (let [key, value] of Object.entries(defaults)) {
+        totalSettings.set(key, value);
+    }
+    let sets = serverSettings.get(guildId);
+    if (sets !== undefined) {
+        for (let [key, value] of Object.entries(sets)) {
+            totalSettings.set(key, value);
+        }
+    }
+    totalSettings.forEach((value, key) => {
+        output += `${key}: ${value}\n`;
+    });
+    utils.sendMessage(channelId, output);
 };
 
 module.exports.getWorkPayout = async (message) => {
